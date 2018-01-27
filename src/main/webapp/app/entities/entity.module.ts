98e4d43e61335f4a7227fd256765da3fed0925dd
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2018-01-27 17:11:15 +0100

    update

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,21 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { PkedziennikUczenModule } from './uczen/uczen.module';
+import { PkedziennikKlasaModule } from './klasa/klasa.module';
+import { PkedziennikSalaModule } from './sala/sala.module';
+import { PkedziennikZasobyModule } from './zasoby/zasoby.module';
+import { PkedziennikNauczycieleModule } from './nauczyciele/nauczyciele.module';
+import { PkedziennikWiadomosciModule } from './wiadomosci/wiadomosci.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        PkedziennikUczenModule,
+        PkedziennikKlasaModule,
+        PkedziennikSalaModule,
+        PkedziennikZasobyModule,
+        PkedziennikNauczycieleModule,
+        PkedziennikWiadomosciModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
